5068253bc7c7c596a9780c3ce08b043ee818806f
Nikunj <nikunj.jain@atos.net>
2018-07-30 18:29:47 +0530

    variable updated to behavior

--- src/RxCustomBindings.swift
+++ src/RxCustomBindings.swift
@@ -16,76 +16,76 @@ infix operator =>
 
 //MARK: Bidirectional bindings
 
-public func <-> <T>(property: ControlProperty<T>?, variable: Variable<T>) -> Disposable {
-    return bidirectionalBinding(property: property, variable: variable)
+public func <-> <T>(property: ControlProperty<T>?, behaviorRelay: BehaviorRelay<T>) -> Disposable {
+    return bidirectionalBinding(property: property, behaviorRelay: behaviorRelay)
 }
 
-public func bidirectionalBinding<T>(property: ControlProperty<T>?, variable: Variable<T>) -> Disposable {
+public func bidirectionalBinding<T>(property: ControlProperty<T>?, behaviorRelay: BehaviorRelay<T>) -> Disposable {
     if let p = property {
-        let bindToUIDisposable = variable => p
-        let bindToVariable = p.asDriver().asObservable().subscribe(onNext: { n in
-            variable.value = n
+        let bindToUIDisposable = behaviorRelay => p
+        let bindToBehaviorRelay = p.asDriver().asObservable().subscribe(onNext: { n in
+            behaviorRelay.accept(n)
         }, onCompleted:  {
             bindToUIDisposable.dispose()
         })
-        return CompositeDisposable(bindToUIDisposable, bindToVariable)
+        return CompositeDisposable(bindToUIDisposable, bindToBehaviorRelay)
     }
     return Disposables.create()
 }
 
 //MARK: Directional binding to ControlProperty
 
-public func bindToProperty<T>(variable: Variable<T>, property: ControlProperty<T>?) -> Disposable {
+public func bindToProperty<T>(behaviorRelay: BehaviorRelay<T>, property: ControlProperty<T>?) -> Disposable {
     if let p = property {
-        let bindToUIDisposable = variable.asDriver().asObservable().bind(to: p)
+        let bindToUIDisposable = behaviorRelay.asDriver().asObservable().bind(to: p)
         return bindToUIDisposable
     }
     return Disposables.create()
 }
 
-public func => <T>(variable: Variable<T>, property: ControlProperty<T>?) -> Disposable {
+public func => <T>(behaviorRelay: BehaviorRelay<T>, property: ControlProperty<T>?) -> Disposable {
     if let p = property {
-        return bindToProperty(variable: variable, property: p)
+        return bindToProperty(behaviorRelay: behaviorRelay, property: p)
     }
     return Disposables.create()
 }
 
 //MARK: Directional binding to Observer
 
-public func bindToObserver<O,T> (variable: Variable<T>, observer: O?, disposeBag: DisposeBag) where O : ObserverType, O.E == T? {
+public func bindToObserver<O,T> (behaviorRelay: BehaviorRelay<T>, observer: O?, disposeBag: DisposeBag) where O : ObserverType, O.E == T? {
     if let obs = observer {
-        variable.asDriver().asObservable().bind(to: obs).disposed(by: disposeBag)
+        behaviorRelay.asDriver().asObservable().bind(to: obs).disposed(by: disposeBag)
     }
 }
 
-public func bindToObserver<O,T> (variable: Variable<T>, observer: O?, disposeBag: DisposeBag) where O : ObserverType, O.E == T {
+public func bindToObserver<O,T> (behaviorRelay: BehaviorRelay<T>, observer: O?, disposeBag: DisposeBag) where O : ObserverType, O.E == T {
     if let obs = observer {
-        variable.asDriver().asObservable().bind(to: obs).disposed(by: disposeBag)
+        behaviorRelay.asDriver().asObservable().bind(to: obs).disposed(by: disposeBag)
     }
 }
 
-public func bindToObserver<O,T> (variable: Variable<T>, observer: O?) where O : ObserverType, O.E == T {
+public func bindToObserver<O,T> (behaviorRelay: BehaviorRelay<T>, observer: O?) where O : ObserverType, O.E == T {
     if let obs = observer {
-        _ = variable.asDriver().asObservable().bind(to: obs)
+        _ = behaviorRelay.asDriver().asObservable().bind(to: obs)
     }
 }
 
-public func bindToObserver<O,T> (variable: Variable<T>, observer: O?) where O : ObserverType, O.E == T? {
+public func bindToObserver<O,T> (behaviorRelay: BehaviorRelay<T>, observer: O?) where O : ObserverType, O.E == T? {
     if let obs = observer {
-        _ = variable.asDriver().asObservable().bind(to: obs)
+        _ = behaviorRelay.asDriver().asObservable().bind(to: obs)
     }
 }
 
-public func => <O,T>(variable: Variable<T>, observer: O?) where O : ObserverType, O.E == T {
-    bindToObserver(variable: variable,observer: observer)
+public func => <O,T>(behaviorRelay: BehaviorRelay<T>, observer: O?) where O : ObserverType, O.E == T {
+    bindToObserver(behaviorRelay: behaviorRelay,observer: observer)
 }
 
-public func => <O,T>(variable: Variable<T>, observer: O?) where O : ObserverType, O.E == T? {
-    bindToObserver(variable: variable,observer: observer)
+public func => <O,T>(behaviorRelay: BehaviorRelay<T>, observer: O?) where O : ObserverType, O.E == T? {
+    bindToObserver(behaviorRelay: behaviorRelay,observer: observer)
 }
 
 //MARK: Directional binding onNext closure
 
-public func bindOnNext<T>( _ variable: Variable<T>, _ onNext: @escaping (T) -> (Void)) {
-    _ = variable.asDriver().asObservable().bind(onNext: onNext)
+public func bindOnNext<T>( _ behaviorRelay: BehaviorRelay<T>, _ onNext: @escaping (T) -> (Void)) {
+    _ = behaviorRelay.asDriver().asObservable().bind(onNext: onNext)
 }
