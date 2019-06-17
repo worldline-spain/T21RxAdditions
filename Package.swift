a3b653a1931096f2ac82ffafea2c9d00868717c2
Anan Sadiya <anan.sadiya@worldline.com>
2019-06-17 11:27:11 +0200

    First approach Swift Package Manager integration.

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "t21_rxadditions_ios",
+    products: [
+        .library(
+            name: "t21_rxadditions_ios",
+            targets: ["T21RxAdditions"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.0.1")
+    ],
+    targets: [
+        .target(
+            name: "t21_rxadditions_ios",
+            dependencies: ["RxSwift"],
+            path: "./src"),
+    ]
+)
